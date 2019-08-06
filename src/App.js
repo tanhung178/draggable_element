6659d8825af8bdf0194ebbccf2f5007ba617abc1
vantanhung2000 <vantanhung2000@gmail.com>
2019-08-06 11:37:35 +0700

    update component modal-image and slideshow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import DraggableElement from './components/draggable-element/draggable-element';
+// Kết hợp Modal Image trong DraggableElement
+import ModalImage from './components/modal-image/modal-image';
 
+// Kết Slideshow trong DraggableElement
+import Slideshow from './components/slideshow/slideshow';
+
+import img1 from './images/01.jpg';
+import img2 from './images/02.jpg';
+import img3 from './images/03.jpg';
+import img4 from './images/04.jpg';
+import img5 from './images/05.jpg';
+import img6 from './images/06.jpg';
+
+const collection = [
+  { src: img1, caption: "Caption one" },
+  { src: img2, caption: "Caption two" },
+  { src: img3, caption: "Caption three" },
+  { src: img4, caption: "Caption four" },
+  { src: img5, caption: "Caption five" },
+  { src: img6, caption: "Caption six" },
+];
 
 function App() {
   return (
@@ -21,6 +40,36 @@ function App() {
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
+      </DraggableElement>
+
+      <DraggableElement
+        title={`Click here to move`}
+        width={`400`}
+        height={`400`}
+        top={`450`}
+        left={`200`}
+      >
+        <h3>Modal Image</h3>
+        <ModalImage
+          src={img6}
+          alt={`This is one of beautiful castle`}
+          ratio={`3:2`}
+        />
+      </DraggableElement>
+
+      <DraggableElement
+          title={`Click here to move`}
+          width={`400`}
+          height={`450`}
+          top={`900`}
+          left={`200`}
+        >
+        <h3>Slideshow</h3>
+        <Slideshow
+          input={collection}
+          ratio={`3:2`}
+          mode={`manual`}
+        />
       </DraggableElement>
 
     </div>
